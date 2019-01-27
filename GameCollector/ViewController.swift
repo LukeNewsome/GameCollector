2809e2c8e0d402c5a68685d1beb667fe34e18912
Luke Newsome <luke@newsome.org>
2019-01-27 14:42:02 -0600

    Initial Commit

---
+++ GameCollector/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GameCollector
+//
+//  Created by Luke Newsome on 1/27/19.
+//  Copyright © 2019 Zappy Code. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
