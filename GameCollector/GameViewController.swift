f19f536befa99b6051cbcc476fdcefa9991c31e0
Luke Newsome <luke@newsome.org>
2019-01-27 16:33:38 -0600

    Added code for buttons and outlets

---
+++ GameCollector/GameViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GameViewController.swift
+//  GameCollector
+//
+//  Created by Luke Newsome on 1/27/19.
+//  Copyright © 2019 Zappy Code. All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var gameImageView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func photosTapped(_ sender: Any) {
+    }
+    @IBAction func cameraTapped(_ sender: Any) {
+    }
+    @IBAction func addTapped(_ sender: Any) {
+    }
+    
+   
+    
+}
